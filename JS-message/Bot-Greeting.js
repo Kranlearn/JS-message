2d229341702222fb90675f95fda0fe295b799cb8
Christophe <christophesilvina@gmail.com>
2026-03-31 19:18:10 +0200

    Add files via upload

---
+++ JS-message/Bot-Greeting.js
@@ -0,0 +1,16 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName =" Jhon";
+let botLocation = "beetween two chair";
+let favoriteLanguage = "Javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " +  favoriteLanguage + ".");
+
+let codingFact = " Interesting and popular fact of programming indeed " + favoriteLanguage + ".";
+console.log(codingFact);
+codingFact = " What's old is new again " + favoriteLanguage + ".";
+console.log(codingFact);
+codingFact = " Any logic that can be written with loops and iterations, you can write it recursively with just functions as well! " + favoriteLanguage + ".";
+console.log(codingFact);
+console.log( "It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
